62b81a66cd484f7f16572ae7f66fb068846cf916
Lhaura <laurarodriguez154@yahoo.com>
2022-10-25 19:58:24 -0400

    Routes Complete

--- models/index.js
+++ models/index.js
@@ -6,11 +6,35 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onUpdate: "CASCADE",
+  onDelete: "CASCADE",
+})
+
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onUpdate: "CASCADE",
+  onDelete: "CASCADE",
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  onUpdate: "CASCADE",
+  onDelete: "SET NULL",
+})
+
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  onUpdate: "CASCADE",
+  onDelete: "SET NULL",
+})
+
+
 
 module.exports = {
   Product,
