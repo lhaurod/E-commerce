58c1f6461151709a18a784cbe04bdb868a03c24a
Lhaura <laurarodriguez154@yahoo.com>
2022-10-25 19:55:52 -0400

    Delete route and complete it

--- models/Category.js
+++ models/Category.js
@@ -1,12 +1,23 @@
 const { Model, DataTypes } = require('sequelize');
 
-const sequelize = require('../../config/connection.js');
+const sequelize = require('../config/connection.js');
 
-class Category extends Model {}
+class Category extends Model { }
 
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+      onDelete: 'CASCADE',
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+
+    }
   },
   {
     sequelize,
@@ -14,7 +25,9 @@ Category.init(
     freezeTableName: true,
     underscored: true,
     modelName: 'category',
+
   }
 );
 
 module.exports = Category;
+
