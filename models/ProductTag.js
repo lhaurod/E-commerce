c96513cd12c859bb4c27343a2e17a1cfe5aa4a55
Lhaura <laurarodriguez154@yahoo.com>
2022-10-25 19:56:49 -0400

    Set up sequelize

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,20 +1,52 @@
+// import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
+// import our database connection from config.js
+const sequelize = require('../config/connection');
 
-const sequelize = require('../../config/connection');
+// Initialize Product model (table) by extending off Sequelize's Model class
+class Product extends Model { }
 
-class ProductTag extends Model {}
-
-ProductTag.init(
+// set up fields and rules for Product model
+Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      isDecimal: true,
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      isInt: true,
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+      onDelete: "CASCADE",
+      onUpdate: "CASCADE"
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product_tag',
+    modelName: 'product',
   }
 );
 
-module.exports = ProductTag;
+module.exports = Product;
